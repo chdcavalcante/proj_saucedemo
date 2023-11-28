4dfca5f38cde5e8eff25b383af4c6815db7e39f2
chdcavalcante <carloshdias.cavalcante@gmail.com>
2023-11-28 19:21:58 -0300

    primeiro commit

--- src/test/java/Proj/aut/saucedemo/runner/Executa.java
+++ src/test/java/Proj/aut/saucedemo/runner/Executa.java
@@ -1,10 +1,9 @@
 package Proj.aut.saucedemo.runner;
 
-import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
